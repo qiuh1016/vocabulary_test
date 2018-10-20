b950b9341e71ec6b7f6fede2ea54e8533f45dd4f
Qiu Hong <qiuh1016@126.com>
2018-10-20 14:35:46 +0800

    1

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('koa-router')()
 const util = require('./util/util')
 
+let map = {}
+
 router.get('/', async (ctx, next) => {
   await ctx.render('index')
 })
@@ -10,10 +12,26 @@ router.get('/word', async (ctx, next) => {
   let num = ctx.query.num || 1
   let words = require(`../vocabulary/${part}/${num}`)
 
+  if (!map[part + num]) {
+    map[part + num] = []
+  } else if (map[part + num].length == words.length) {
+    map[part + num] = []
+    return ctx.body = {
+      code: 0,
+      msg: '一遍已完成'
+    }
+  }
+
   let random = util.randomNumber(0, words.length - 1)
+  while (map[part + num].indexOf(random) != -1) {
+    random = util.randomNumber(0, words.length - 1)
+  }
+  map[part + num].push(random)
+
   console.log(random)
   let word = words[random]
-  ctx.body ={
+  ctx.body = {
+    code: 1,
     word: word,
     wordUrl: `http://dict.youdao.com/dictvoice?audio=${word.word}`
   }
--- src/components/Reading.vue
+++ src/components/Reading.vue
@@ -31,7 +31,7 @@ export default {
   data() {
     return {
       message: "Hello Vue!",
-      part: 'reading',
+      part: "reading",
       num: 0,
       word: {
         word: "",
@@ -49,77 +49,83 @@ export default {
     };
   },
   mounted() {
-    this.getWord()
+    this.getWord();
   },
   methods: {
     getWord() {
-      axios.get("/word", {
-          params: { part: this.part, num: this.num },
-        }).then(res => {
-        this.cover.word = this.defaultCover.word
-        this.cover.translation = this.defaultCover.translation
-        this.word = res.data.word
-        this.audioUrl = res.data.wordUrl
-      });
+      axios
+        .get("/word", {
+          params: { part: this.part, num: this.num }
+        })
+        .then(res => {
+          if (res.data.code == 0) {
+            alert(res.data.msg)
+          } else if (res.data.code == 1) {
+            this.cover.word = this.defaultCover.word;
+            this.cover.translation = this.defaultCover.translation;
+            this.word = res.data.word;
+            this.audioUrl = res.data.wordUrl;
+          }
+        });
     },
     openTranslateCover() {
-      this.cover.translation = false
+      this.cover.translation = false;
     },
     openWordCover() {
-      this.cover.word = false
+      this.cover.word = false;
     }
   }
 };
 </script>
 <style scoped>
-  .page {
-    justify-content: center;
-    align-items: center;
-  }
+.page {
+  justify-content: center;
+  align-items: center;
+}
 
- .select {
-    width: 130px;
-    height: 30px;
-    border: 1px solid black;
-    border-radius: 0;
-    padding: 0 5px; 
-    background: #fff;
-  }
+.select {
+  width: 130px;
+  height: 30px;
+  border: 1px solid black;
+  border-radius: 0;
+  padding: 0 5px;
+  background: #fff;
+}
 
-  .input {
-    width: 120px;
-    height: 28px;
-    border: 1px solid black;
-    border-radius: 0;
-    padding: 0 5px; 
-    background: #fff;
-  }
-  .next {
-    width: 120px;
-    height: 44px;
-    border-radius: 22px;
-    border: 1px solid black;
-    background: #fff;
-  }
+.input {
+  width: 120px;
+  height: 28px;
+  border: 1px solid black;
+  border-radius: 0;
+  padding: 0 5px;
+  background: #fff;
+}
+.next {
+  width: 120px;
+  height: 44px;
+  border-radius: 22px;
+  border: 1px solid black;
+  background: #fff;
+}
 
-  .word {
-    height: 60px;
-    line-height: 60px;
-    margin: 0;
-  }
+.word {
+  height: 60px;
+  line-height: 60px;
+  margin: 0;
+}
 
-  .translation {
-    height: 60px;
-    line-height: 60px;
-    margin: 0;
-  }
+.translation {
+  height: 60px;
+  line-height: 60px;
+  margin: 0;
+}
 
-  .cover {
-    background: #2c3e50;
-    border: 1px solid black;
-  }
+.cover {
+  background: #2c3e50;
+  border: 1px solid black;
+}
 
-  audio {
-    margin: 15px 0;
-  }
+audio {
+  margin: 15px 0;
+}
 </style>
